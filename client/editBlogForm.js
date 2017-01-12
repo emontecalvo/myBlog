ba2a0ad2c640a1226f947ecc4188e0df8bfb124f
emontecalvo <emilymontecalvo@gmail.com>
2017-01-12 10:33:50 -0800

    edit button text working

--- client/editBlogForm.js
+++ client/editBlogForm.js
@@ -14,15 +14,16 @@ const editBlogForm = ({blogToEdit, editBlogPost}) => {
               let content = e.target.content.value
               let tags = e.target.tags.value
               editBlogPost(title, content, tags)
-              e.target.title.value = ''
-              e.target.content.value = ''
-              e.target.tags.value = ''
+
+              e.target.title.value = 'hello'
+              e.target.content.value = blogToEdit.content
+              e.target.tags.value = blogToEdit.tags
             }}
           >
 
-      <input type="text" placeholder={blogToEdit.title} name="title" />
-      <textarea rows="4" cols="50" placeholder={blogToEdit.content} name="content" />
-      <input type="text" placeholder={blogToEdit.tags} name="tags" />
+      <input type="text" name="title" defaultValue={blogToEdit.title}/>
+      <textarea rows="4" cols="50" name="content" defaultValue={blogToEdit.content}/>
+      <input type="text" name="tags" />
 
     <button type="submit">
       Save Edit
